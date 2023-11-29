0681f97b0b604bfd6d043586dcc4be6b6c196b26
KSuondProject <70426429+KSuondProject@users.noreply.github.com>
2023-11-29 08:53:16 +0100

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -20,8 +20,8 @@
       </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
-      <h1 class="now-playing__idle-heading" {{ currentTime }}></h1>
-      <h2 class="now-playing__idle-heading" {{ currentDate }}></h2>
+      <h1 class="now-playing__idle-heading"> {{ currentTime }} </h1>
+      <h2 class="now-playing__idle-heading"> {{ currentDate }} </h2>
     </div>
   </div>
 </template>
