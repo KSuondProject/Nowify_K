89d09d10a3205ed5a9c11d9867583fd8d2f2bd2f
KSuondProject <70426429+KSuondProject@users.noreply.github.com>
2023-11-29 16:46:22 +0100

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -84,7 +84,7 @@ export default {
       const now = new Date();
       // Format the date and time
       const formattedDate = now.toLocaleString('it-IT', {timeZone: 'CET', weekday: 'long', month: 'long', day: 'numeric'});
-      const formattedTime = now.toLocaleTimeString('it-IT', {hour:'numeric', minute:'numeric'});
+      const formattedTime = now.toLocaleTimeString('it-IT');
       // Update the currentDatetime property
       this.currentDate = formattedTime;
       this.currentDate = formattedDate;
